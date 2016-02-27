d11e9463a90a9a7f0c93b4450fa3f1c63a03ab5e
HL <hlere@foxmail.com>
2016-02-27 12:11:11 +0800

    解决json文件，打包入压缩文件后不能写入问题

---
+++ main.js
@@ -0,0 +1,21 @@
+
+allWindows =new Array();
+
+exports.allWindows =allWindows;
+
+exports.mainWindom;
+
+exports.smInintState=0;
+
+var path = require('path');
+var nwPath = process.execPath;
+var nwDir = path.dirname(nwPath);
+
+exports.nwPath=nwDir
+
+// var fs=require("fs");
+// fs.readFile("./config/shortMessageData.json","UTF-8");
+
+
+
+
